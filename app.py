f216508a19cf52614cdcda9db2471660b6face14
Hisham Ali Alshami <ahishamali10@gmail.com>
2024-03-06 11:54:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def hello_world():
 if __name__ == '__main__':
     # Note: Google Cloud Run will set the PORT environment variable to tell you what port to listen on
     import os
-    port = int(os.environ.get('PORT', 8080))  # Default to 8080 if PORT not set
+    port = int(os.environ.get('PORT', 80))  # Default to 8080 if PORT not set
     app.run(debug=True, host='0.0.0.0', port=port)
