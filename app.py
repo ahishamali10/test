4ab86e5d2aea635719ba1d0b004fbfd71f1f46e8
Hisham Ali Alshami <ahishamali10@gmail.com>
2024-03-06 11:47:46 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    # Note: Google Cloud Run will set the PORT environment variable to tell you what port to listen on
+    import os
+    port = int(os.environ.get('PORT', 8080))  # Default to 8080 if PORT not set
+    app.run(debug=True, host='0.0.0.0', port=port)
